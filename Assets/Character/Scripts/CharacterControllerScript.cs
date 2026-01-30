7db1e85be07ae5849f7d4bedc10b719e20259fcb
Tillomaticus <Till.Ikemann@gmail.com>
2026-01-30 20:22:03 +0100

    feat: implemented shapeshifting targeting;

--- Assets/Character/Scripts/CharacterControllerScript.cs
+++ Assets/Character/Scripts/CharacterControllerScript.cs
@@ -7,6 +7,7 @@ public class CharacterControllerScript : MonoBehaviour
 
     [SerializeField]
     CharacterVisual _characterVisual;
+    [SerializeField] MaskingSystem _maskingSystem;
 
     CharacterDirection _characterDirection;
 
