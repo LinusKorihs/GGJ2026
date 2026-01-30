3e10db3902c524b449e4775a820ed6ed7b44b3a4
Linus Theuringer <Linustheuringer@web.de>
2026-01-30 21:07:23 +0100

    NPC walk through each other

--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -50,6 +50,9 @@ public class NPCMovement : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
+        int npcLayer = LayerMask.NameToLayer("NPC");
+        Physics2D.IgnoreLayerCollision(npcLayer, npcLayer, true);
+
         // Start room: find closest waypoint you are allowed to use
         var startWp = GetClosestAllowedWaypoint(transform.position);
         currentRoomId = startWp != null ? startWp.roomId : "";
